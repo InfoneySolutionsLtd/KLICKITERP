4c029862f4b93c2f2c273d1f937101c1f0565e37
Newton Karani <newtonkarani98@gmail.com>
2026-09-23 20:24:43 +0300

    Minio impl

---
+++ gitops/postgres-init/001-create-erp-roles.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -eu
+
+: "${DB_PASSWORD:?DB_PASSWORD must be set for the ERP database role}"
+: "${DB_MIGRATION_PASSWORD:?DB_MIGRATION_PASSWORD must be set for the migration role}"
+
+psql \
+  --username "$POSTGRES_USER" \
+  --dbname "$POSTGRES_DB" \
+  --set=ON_ERROR_STOP=1 \
+  --set=app_password="$DB_PASSWORD" \
+  --set=migration_password="$DB_MIGRATION_PASSWORD" <<'SQL'
+SELECT format('CREATE ROLE kfe_app LOGIN PASSWORD %L', :'app_password')
+WHERE NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'kfe_app')
+\gexec
+
+SELECT format('CREATE ROLE kfe_migrate LOGIN PASSWORD %L', :'migration_password')
+WHERE NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'kfe_migrate')
+\gexec
+
+SELECT format('CREATE ROLE kfe_license LOGIN PASSWORD %L', :'migration_password')
+WHERE NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'kfe_license')
+\gexec
+
+ALTER ROLE kfe_app PASSWORD :'app_password';
+ALTER ROLE kfe_migrate PASSWORD :'migration_password';
+ALTER ROLE kfe_license PASSWORD :'migration_password';
+SQL
