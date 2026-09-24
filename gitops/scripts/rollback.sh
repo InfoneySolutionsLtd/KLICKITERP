2e10ed8f410500f059450363acd3744c03ba8eff
Newton Karani <newtonkarani98@gmail.com>
2026-09-24 09:44:26 +0300

    swarm

--- gitops/scripts/rollback.sh
+++ gitops/scripts/rollback.sh
@@ -20,6 +20,7 @@ export KFE_VERSION ERP_IMAGE_REGISTRY
 STACK_FILE="$(mktemp)"
 trap 'rm -f "$STACK_FILE"' EXIT
 "${compose[@]}" config > "$STACK_FILE"
+sed -i '/^name: /d' "$STACK_FILE"
 sed -i -E 's/(published: )"([0-9]+)"/\1\2/g' "$STACK_FILE"
 docker stack deploy --with-registry-auth --prune -c "$STACK_FILE" klickit-erp
 docker stack services klickit-erp
